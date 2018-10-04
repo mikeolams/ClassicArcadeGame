77df480a489afe8df4907c558f72aac54f2d4555
Michael <mikeolams@yahoo.com>
2018-10-04 23:02:42 +0100

    first 3

--- README.md
+++ README.md
@@ -1,47 +1,29 @@
-## Welcome to GitHub Pages
+## Welcome to my frontend-nanodegree-arcade-game on GitHub
+===========================================================
 
-You can use the [editor on GitHub](https://github.com/mikeolams/ClassicArcadeGame/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
+Game Procedure
+==============
+You will be required to use the arrow keys to navigate the roaming enemy to reach the blue top block of the ClassicArcadeGame to win or complete the game successfully.
 
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
 
-### Markdown
+### Information
+===============
 
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+You may bear in mind we will continue to add other features in the future.
 
-```markdown
-Syntax highlighted code block
 
-# Header 1
-## Header 2
-### Header 3
-
-- Bulleted
-- List
-
-1. Numbered
-2. List
-
-**Bold** and _Italic_ and `Code` text
-
-[Link](url) and ![Image](src)
+[Link](https://github.com/mikeolams/ClassicArcadeGame/settings)
 ```
 
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
-
-### Jekyll Themes
-
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/mikeolams/ClassicArcadeGame/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
-
 ### Support or Contact
+======================
 
-Having trouble with Pages? Check out our [documentation](https://help.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
-
-
-
+In event of any issue please twist or email me directly and we’ll help you sort it out.
 
 
 
 
+### Other Guiding details for building the game
 
 frontend-nanodegree-arcade-game
 ===============================
@@ -49,9 +31,3 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
-
-
-
-Game Procedure
-===============================
-
--- js/app.js
+++ js/app.js
@@ -1,203 +1,159 @@
 // Enemies our player must avoid
-var Enemy = function(corX,corY,velocity) {
+var Enemy = function(corX, corY, velocity) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.corX =corX;
-    this.corY=corY;
-    this.velocity =velocity;
+    this.corX = corX;
+    this.corY = corY;
+    this.velocity = velocity;
 };
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-let count =0;
+let count = 0;
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-       this.corX += this.velocity * dt;
-       if(this.corX > 505) {
-            count+=1
-            if (count<4){
-                this.corX = Math.floor(Math.random())*(-200);
-                this.velocity = 400 + Math.floor(Math.random() *300);
-                // console.log(`1stset`, this.corX,this.corY); 
-            }
-             // delay the enemy at old number by 3sec;
-            if ( count%2 == 0 ){
-                this.corX = -300;
-                this.velocity =0;
-                setTimeout(() => {
+    this.corX += this.velocity * dt;
+    if (this.corX > 505) {
+        count += 1
+        if (count < 4) {
+            this.corX = Math.floor(Math.random()) * (-200);
+            this.velocity = 400 + Math.floor(Math.random() * 300);
+        }
+        // delay the enemy at old number by 3sec;
+        if (count % 2 == 0) {
+            this.corX = -300;
+            this.velocity = 0;
+            setTimeout(() => {
                 this.velocity = 393.5;
                 this.corX = -200;
             }, 3000);
-            }else {
-            this.corX = Math.floor(Math.random())*(-300);
-            // console.log(`others`, this.corX, this.corY);
-            // console.log(count);
-            this.velocity = 300 + Math.floor(Math.random() *50);
-            }
-            if (count ===10){count =0;
-            }
-            
-        } 
-
+        } else {
+            this.corX = Math.floor(Math.random()) * (-300);
+            this.velocity = 300 + Math.floor(Math.random() * 50);
+        }
+        if (count === 10) {
+            count = 0;
+        }
+    }
 };
-
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.corX, this.corY);
 };
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class PlayerImage {
-    constructor(corX,corY){
+    constructor(corX, corY) {
         this.corX = corX;
         this.corY = corY;
         this.person = 'images/char-pink-girl.png';
-        // this.person ='images/char-boy.png';
-        // this.person ='char-cat-girl.png';
-
-        // char-princess-girl.png char-pink-girl.png   char-boy.png char-horn-girl.png char-cat-girl.png
     }
-
-    update(){
-         // if (this.corY == (for (enemy of allEnemies){allEnemies[item]} )) {console.log(`popo`)}
-        // if ((for (enemy of allEnemies){allEnemies[enemy]} )) {console.log(`popo`)}
-         if (this.corY >=200) {
-            // console.log(`coordinate ${this.corX} ${this.corY}`)
-            // reset();
-
-            for(let i=0; i<allEnemies.length; i++){if (true){let count =0; 
+    update() {
+        // reset();
+        for (let i = 0; i < allEnemies.length; i++) {
+            if (true) {
+                let count = 0;
                 count++;
-                // allEnemies[i].corY;
-                // allEnemies[i].corX;
-                if(allEnemies[i].corY == player.corY ){console.log(`y`, count,i)}
-                if(allEnemies[i].corX == player.corX ){console.log(`x`, count,i)}
-
-                // console.log(count,i, allEnemies[i].corY, allEnemies[i].corX)
-            }}
-                // player.corX;
-                // player.corY;
-
+                if (allEnemies[i].corX >= player.corX - 12) {
+                    if (allEnemies[i].corY == 64 && (player.corY > 70 && player.corY < 110)) {
+                        for (let j = 0; j < 500; j += 100) {
+                            if (player.corX == j) {
+                                player.corX = j;
+                                player.corY = 435
+                            }
+                        }
+                    } else if (allEnemies[i].corY == 147 && (player.corY > 150 && player.corY < 186)) {
+                        for (let j = 0; j < 500; j += 100) {
+                            if (player.corX == j) {
+                                player.corX = j;
+                                player.corY = 435
+                            }
+                        }
+                    } else if (allEnemies[i].corY == 230 && (player.corY > 236 && player.corY < 244)) {
+                        for (let j = 0; j < 500; j += 100) {
+                            if (player.corX == j) {
+                                player.corX = j;
+                                player.corY = 435
+                            }
+                        }
+                    } else { /*do nothing*/ }
+                }
+            }
         }
-            if (player.corY == -9){
-            console.log(`well done`);
+        if (player.corY == -9) {
             swal({
                 title: "Good job! Congratulations!!!",
-                text: "You made great effort! \n by completed the game after moves \n Well done!!!",
+                text: "You made great effort! \n by completed the game after careful moves \n Well done!!!",
                 icon: "success",
             });
-                 setTimeout(() => {
-            player.corX =200;
+            setTimeout(() => {
+                player.corX = 200;
                 player.corY = 435;
-        }, 1000);
-            }
-
-       }
-
-
-    render(){
+            }, 1000);
+        }
+    }
+    render() {
         ctx.drawImage(Resources.get(this.person), this.corX, this.corY);
     }
-
-    handleInput(input){
-        if(input == "left"){ 
-            // console.log(this.corX);
-           if (this.corX >0 && this.corX<=400 ){
-            // if(this.corX ==400){this.corX =400;}
-            // if(this.corX==0){this.corX =0;}
-            this.corX -= 100;
-        } 
-        }else if(input =='right'){
-            // console.log(this.corX);
-            if (this.corX >=0 && this.corX<400 ){
-                // if(this.corX ==400){this.corX =400;}
-            // if(this.corX ==0){this.corX =0;}
-            this.corX += 100;
+    handleInput(input) {
+        if (input == "left") {
+            if (this.corX > 0 && this.corX <= 400) {
+                this.corX -= 100;
             }
-
-
-        }else if (input =='down'){
-            // console.log(this.corY);
-            if ((this.corY >=-9) && this.corY<411){
-            if (this.corY==435){
-                this.corY += 110;
-                // console.log(this.corY);
-            }else if(this.corY==73){
-                this.corY += 82;
-                // console.log(this.corY);
-            }else{
-                this.corY += 84;
-                // console.log(this.corY);
+        } else if (input == 'right') {
+            if (this.corX >= 0 && this.corX < 400) {
+                this.corX += 100;
             }
+        } else if (input == 'down') {
+            if ((this.corY >= -9) && this.corY < 411) {
+                if (this.corY == 435) {
+                    this.corY += 110;
+                } else if (this.corY == 73) {
+                    this.corY += 82;
+                } else {
+                    if (this.corY >= 400) {
+                        //do thing
+                    } else {
+                        this.corY += 84;
+                    }
+                }
             }
-        }else if (input =='up'){
-            // console.log(this.corY);
-            if ((this.corY >-9) && this.corY<=435 ){
-            if (this.corY==435){
-                console.log(this.corY);
-                this.corY -= 110;
-            }else if(this.corY==73){
-                this.corY -= 82;
-                console.log(this.corY);
-            }else{
-                this.corY -= 84;
-                console.log(this.corY);
+        } else if (input == 'up') {
+            if ((this.corY > -9) && this.corY <= 435) {
+                if (this.corY == 435) {
+                    this.corY -= 110;
+                } else if (this.corY == 73) {
+                    this.corY -= 82;
+                } else {
+                    this.corY -= 84;
+                }
             }
-        }
-        }else{/*do nothing for*/}
+        } else { /*do nothing for*/ }
     }
 }
-
-// if(allowedKeys[e.keyCode] == "left"){ 
-//             this.corX += 50;
-//         }else if(allowedKeys[e.keyCode] =='right'){
-//             this.corX -= 50;
-//         }else if (allowedKeys[e.keyCode]=='down'){
-//             this.corY += 50;
-//         }else if (allowedKeys[e.keyCode]=='up'){
-//             this.corY -= 50;
-//         }else{let u=1;}
-//     }
-// if(this.allowedKeys[39]){ 
-//             this.corX += 50;
-//         }else if(this.allowedKeys[37]){
-//             this.corX -= 50;
-//         }else if (this.allowedKeys[40]){
-//             this.corY += 50;
-//         }else if (this.allowedKeys[38]){
-//             this.corY -= 50;
-//         }else{let u=1;}
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-
 //setting the array list for enemies
 const allEnemies = [];
 // Instantiate each enemy objects
-const enemyOne = new Enemy(-100,64, Math.random()* 700);
-    allEnemies.push(enemyOne),
-      enemyTwo = new Enemy(-150, 147, Math.random()* 700),
-allEnemies.push(enemyTwo),
-      enemyThree = new Enemy(-50, 230, Math.random()* 700),
-allEnemies.push(enemyThree),
-      enemyFour = new Enemy(-300,64, Math.random()* 700),
-allEnemies.push(enemyFour),
-      enemyFive = new Enemy(-150, 230, Math.random()* 700),
-allEnemies.push(enemyFive);
-
+const enemyOne = new Enemy(-100, 64, Math.random() * 700);
+allEnemies.push(enemyOne),
+    enemyTwo = new Enemy(-150, 147, Math.random() * 700),
+    allEnemies.push(enemyTwo),
+    enemyThree = new Enemy(-50, 230, Math.random() * 700),
+    allEnemies.push(enemyThree),
+    enemyFour = new Enemy(-300, 64, Math.random() * 700),
+    allEnemies.push(enemyFour),
+    enemyFive = new Enemy(-150, 230, Math.random() * 700),
+    allEnemies.push(enemyFive);
 // Place the player object in a variable called player
 // Instantiate the player object
-const player = new PlayerImage(200,435);
-
-
-
+const player = new PlayerImage(200, 435);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -207,6 +163,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
