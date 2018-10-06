56e3161dc4709297c83b52fe517f0d8af223ceb5
Michael <mikeolams@yahoo.com>
2018-10-06 14:39:53 +0100

    first edit

--- js/app.js
+++ js/app.js
@@ -1,54 +1,51 @@
-// Enemies our player must avoid
-var Enemy = function(corX, corY, velocity) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.corX = corX;
-    this.corY = corY;
-    this.velocity = velocity;
-};
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//setting global parameters
 let count = 0;
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.corX += this.velocity * dt;
-    if (this.corX > 610) {
-        count += 1
-        if (count < 4) {
-            this.corX = Math.floor(Math.random()) * (-300);
-            this.velocity = 400 + Math.floor(Math.random() * 300);
-        }
-        // delay the enemy at old number by 3sec;
-        if (count % 2 == 0) {
-            this.corX = -300;
-            this.velocity = 0;
-            setTimeout(() => {
-                this.velocity = 393.5;
-            }, 3000);
-        } else {
-            this.corX = Math.floor(Math.random()) * (-300);
-            this.velocity = 400 + Math.floor(Math.random() * 60);
-        }
-        //to create a dynamic velocity variation
-        if (count > 9 && count < 25) {
-            this.velocity *= 2;
-            this.corX = -200;
-        } else if (count == 25) {
-            count = 0;
-        } else {
-            count;
-        }
+// Enemies our player must avoid
+class Enemy {
+    constructor(corX, corY, velocity) {
+        this.sprite = 'images/enemy-bug.png';
+        this.corX = corX;
+        this.corY = corY;
+        this.velocity = velocity;
     }
-};
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.corX, this.corY);
-};
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.corX += this.velocity * dt;
+        if (this.corX > 610) {
+            count += 1
+            if (count < 4) {
+                this.corX = Math.floor(Math.random()) * (-300);
+                this.velocity = 400 + Math.floor(Math.random() * 300);
+            }
+            // delay the enemy at old number by 3sec;
+            if (count % 2 == 0) {
+                this.corX = -300;
+                this.velocity = 0;
+                setTimeout(() => {
+                    this.velocity = 393.5;
+                }, 3000);
+            } else {
+                this.corX = Math.floor(Math.random()) * (-300);
+                this.velocity = 400 + Math.floor(Math.random() * 60);
+            }
+            //to create a dynamic velocity variation
+            if (count > 9 && count < 25) {
+                this.velocity *= 2;
+                this.corX = -200;
+            } else if (count == 25) {
+                count = 0;
+            } else {
+                count;
+            }
+        }
+    };
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.corX, this.corY);
+    };
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
